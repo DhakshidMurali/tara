ff0fc76a743b4e1e2cc8d04cc0b816bd4d336f50
DhakshidMurali <dhakshidmurali.2001@gmail.com>
2024-12-10 20:35:34 +0530

    Fixed Peer Dependency

--- codeApp/tara/components/dashboard/box/departmentListBox.js
+++ codeApp/tara/components/dashboard/box/departmentListBox.js
@@ -1,35 +1,9 @@
 "use client";
-<<<<<<< HEAD
-import {
-  Avatar,
-  Box,
-  Button,
-  Collapse,
-  Grid,
-  Stack,
-  Typography,
-} from "@mui/material";
-=======
 import { Avatar, Box, Button, Grid, Stack, Typography } from "@mui/material";
->>>>>>> dd64d5ba35af4a29e638d731b832199630c785c6
 import { styles } from "../useStyles";
 import { Domain, ExpandMore } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 import { DepartmentList } from "@/public/Sample/data";
-<<<<<<< HEAD
-import { useState } from "react";
-
-export default function DepartmentListBox(props) {
-  const router = useRouter();
-  const departmentList = DepartmentList;
-  const [selectedDropDown, setSelectedDropDownOpen] = useState();
-  const handleDropDownDepartmentList = (value) => {
-    if (value == selectedDropDown) {
-      setSelectedDropDownOpen(-1);
-      return;
-    }
-    setSelectedDropDownOpen(value);
-=======
 
 export default function DepartmentListBox(props) {
   const router = useRouter();
@@ -38,7 +12,6 @@ export default function DepartmentListBox(props) {
   const handleDropDownDepartmentList = () => {
     isDropDownOpen = !isDropDownOpen;
     router.refresh();
->>>>>>> dd64d5ba35af4a29e638d731b832199630c785c6
   };
   return (
     <Grid
@@ -59,86 +32,6 @@ export default function DepartmentListBox(props) {
         {departmentList.map((i, index) => {
           return (
             <Box sx={styles.departmentContainerListBoxStyle}>
-<<<<<<< HEAD
-              <Grid
-                container
-                spacing={2}
-                sx={{
-                  justifyContent: "flex-start",
-                }}
-              >
-                <Grid item xs={12}>
-                  <Stack direction={"row"}>
-                    <Avatar>
-                      <Domain sx={styles.deparmentListIconStyle}></Domain>
-                    </Avatar>
-                    <Typography
-                      variant="h6"
-                      sx={styles.dashboardTextTypographStyle}
-                      paddingLeft={2}
-                    >
-                      {departmentList[index]}
-                    </Typography>
-                    <Button
-                      sx={{ margin: 0 }}
-                      onClick={() => {
-                        handleDropDownDepartmentList(index);
-                      }}
-                    >
-                      <ExpandMore
-                        sx={{ marginLeft: 0, color: "white" }}
-                      ></ExpandMore>
-                    </Button>
-                  </Stack>
-                </Grid>
-                {selectedDropDown == index ? (
-                  <Collapse
-                    in={selectedDropDown == index}
-                    timeout="auto"
-                    orientation="vertical"
-                    unmountOnExit
-                  >
-                    <Grid
-                      item
-                      xs={12}
-                      sx={{
-                        padding: 0,
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <Stack direction={"row"}>
-                        <Typography
-                          variant="h6"
-                          sx={styles.dashboardTextTypographStyle}
-                          paddingLeft={2}
-                        >
-                          {selectedDropDown == index
-                            ? departmentList[index]
-                            : "Hello"}
-                        </Typography>
-                      </Stack>
-                    </Grid>
-                    <Grid item xs={12} sx={{ padding: 0 }}>
-                      <Stack direction={"row"}>
-                        <Typography
-                          variant="h6"
-                          sx={styles.dashboardTextTypographStyle}
-                          paddingLeft={2}
-                        >
-                          {selectedDropDown == index
-                            ? departmentList[index]
-                            : "Hello"}
-                        </Typography>
-                      </Stack>
-                    </Grid>
-                  </Collapse>
-                ) : (
-                  <></>
-                )}
-              </Grid>
-=======
               <Stack direction={"row"}>
                 <Avatar>
                   <Domain sx={styles.deparmentListIconStyle}></Domain>
@@ -159,7 +52,6 @@ export default function DepartmentListBox(props) {
                   ></ExpandMore>
                 </Button>
               </Stack>
->>>>>>> dd64d5ba35af4a29e638d731b832199630c785c6
             </Box>
           );
         })}
