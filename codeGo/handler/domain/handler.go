b78d530d57e8e983a7f57bd80541657911d23d4f
DhakshidMurali <dhakshidmurali2001@gmail.com>
2025-04-12 17:47:46 +0530

    Moved the code files from Tara to CodeApp Folder

--- codeGo/handler/domain/handler.go
+++ codeGo/handler/domain/handler.go
@@ -8,6 +8,8 @@ import (
 func APIRoutes(server *gin.Engine) {
 	domainRoutes := server.Group("/")
 
+	domainRoutes.GET("/get/domainpage/details", domainService.GetDomainPageDetails)
+
 	domainRoutes.GET("/get/list/toolGroupByDomain", domainService.ListToolGroupByDomain)
 
 	domainRoutes.GET("get/list/toolsGroupByDeliveryFormatForTop4Domains", domainService.ListToolsGroupByDeliveryFormatForTop4Domain)
--- codeGo/service/domain/Relation.go
+++ codeGo/service/domain/Relation.go
@@ -5,10 +5,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func GetDomainPageDetails(context *gin.Context) {
+	domainService.ListToolGroupByDomain(context)
+	domainService.ListToolsGroupByDeliveryFormatForTop4Domain(context)
+}
 func ListToolGroupByDomain(context *gin.Context) {
 	domainService.ListToolGroupByDomain(context)
 }
 
 func ListToolsGroupByDeliveryFormatForTop4Domain(context *gin.Context) {
-	domainService.ListToolGroupByDomain(context)
+	domainService.ListToolsGroupByDeliveryFormatForTop4Domain(context)
 }
